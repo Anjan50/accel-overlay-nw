12703494fa3a1a1ea14be52c26ed079f71053a19
Anjan50 <adiyora@ncsu.edu>
2026-01-05 15:25:49 -0600

    refactor: remove old file structure and complete migration
    - Remove legacy files from root directory
    - Complete migration to new organized structure
    - Clean up old deployment scripts and result files
    - Remove duplicate data_parse directory from root

--- rfs/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-mount -o remount,rw /sys
-/usr/local/bin/enable_rfs.sh
-exec iperf3 "$@"
--- rps/enable_rps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-for file in /sys/class/net/*/queues/rx-*/rps_cpus; do \
-    echo f > $file; \
-    echo "$file"; \
-    cat $file; \
-done
-echo 'Enabled RPS on all interfaces'
